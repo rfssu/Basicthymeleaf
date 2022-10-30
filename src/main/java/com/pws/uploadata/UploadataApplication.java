8e373f87ed452bb382956db98a43086d742ddc9f
rfssu <rafi.saifullah.ft20@mail.umy.ac.id>
2022-10-30 22:24:48 +0700

    Persistance and Constructor

---
+++ src/main/java/com/pws/uploadata/UploadataApplication.java
@@ -0,0 +1,13 @@
+package com.pws.uploadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UploadataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pws/uploadata/UploadataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pws.uploadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
