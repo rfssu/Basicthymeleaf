0fad911586caee778376be2c31775f5ad54609f7
rfssu <rafi.saifullah.ft20@mail.umy.ac.id>
2022-10-30 22:43:08 +0700

    Java Class

---
+++ src/main/java/com/pws/uploadata/info.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pws.uploadata;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class info {
+    private String name;
+    private String picture;
+    public info() {
+        super();
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getPicture(){
+        return picture;
+    }
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+    
+}
