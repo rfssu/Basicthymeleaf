c68cfbfd71cafce1c34cb3f9fc2597844c66859e
rfssu <rafi.saifullah.ft20@mail.umy.ac.id>
2022-10-31 14:50:09 +0700

    Update View

---
+++ src/main/java/com/pws/uploadata/DataController.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pws.uploadata;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ *
+ * @author Lenovo
+ */
+
+@Controller
+public class DataController {
+    
+    Requ
+}
