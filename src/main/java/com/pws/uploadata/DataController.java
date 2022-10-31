f86b76a26cfb88e1dcb157682ce354861104f968
rfssu <rafi.saifullah.ft20@mail.umy.ac.id>
2022-10-31 17:16:31 +0700

    Revise

--- src/main/java/com/pws/uploadata/DataController.java
+++ src/main/java/com/pws/uploadata/DataController.java
@@ -5,24 +5,46 @@
  */
 package com.pws.uploadata;
 
+import java.io.IOException;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
 
 /**
  *
- * @author Lenovo
+ * @author ACER
  */
-
 @Controller
 public class DataController {
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String data(
+            @RequestParam(value = "varA") String nama,
+            @RequestParam(value = "varB") String tgl
+    )
     
-    @RequestMapping("/")
-    public String register(){
-        return "info";
+    @RequestMapping("/nexthalaman")
+    //@ResponseBody
+    public String next(@RequestParam(value = "varA") String nama,
+            @RequestParam(value = "varB") String tgl,
+            @RequestParam(value = "foto") MultipartFile gambar,
+            Model kurir
+            
+    )throws IOException{  
+        byte[] img = gambar.getBytes();
+        String base64Image = Base64.encodeBase64String(img);
+        String imglink= "data:image/png;base64,".concat(base64Image);
+        kurir.addAttribute("name", nama);
+        kurir.addAttribute("Bdate", tgl);
+        kurir.addAttribute("Pict", imglink);
+        
+        
+        return "viewpage";
     }
     
-    @PostMapping("/save")
-    public String save(){
-        return "info-view";
-    }
 }
